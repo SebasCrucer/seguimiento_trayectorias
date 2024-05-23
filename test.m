411cf159d9fa3c331721e11b8438290a4e501987
Sebastian <diego.cruz.cervantes@gmail.com>
2024-05-23 11:10:05 -0600

    yaquedo

--- test.m
+++ test.m
@@ -12,13 +12,15 @@ ROI(1:30, 1:end) = 0;
 [mvAccFrames, mvFrames] = getMovement(videoFile, evalRange, filterSigma);
 
 mvAccFrames = mvAccFrames.*ROI;
-matMvFrames = cat(3, mvFrames{:});
+mvFrames = cat(3, mvFrames{:});
 
-for i = 1:size(matMvFrames, 3)
-    matMvFrames(:, :, i) = matMvFrames(:, :, i) .* ROI;
+for i = 1:size(mvFrames, 3)
+    mvFrames(:, :, i) = mvFrames(:, :, i) .* ROI;
 end
 
-createVideoWithFFmpeg(matMvFrames, 'outputs/output.mp4', 30);
+mvFrames = mat2cell(mvFrames, size(mvFrames, 1), size(mvFrames, 2), ones(1, size(mvFrames, 3)));
+
+createVideoWithFFmpeg(mvFrames, 'outputs/output.mp4', 30);
 
 binaryMvAccFrames = mvAccFrames >= 1;
 imwrite(mat2gray(binaryMvAccFrames), 'outputs/accumulated_image.png');
